6f43bcd0f51b3e5147cf62cae5ff93a052ea92aa
Lewis-Peaty <lewis.peaty@gmail.com>
2016-03-24 15:15:54 +0800

    Update flatten_folder_structure.R

--- flatten_folder_structure.R
+++ flatten_folder_structure.R
@@ -9,17 +9,8 @@ for(item in list.files(folder, recursive = TRUE)){
   if(file.copy(from = item,to = folder)){
     print(paste("Successfully copied",item,"to",folder)) 
     if(remove_after_copying){
-      if(file.remove(item)){
-        print(paste("Removed",item))
-      } else{
-         print(paste("Failed to remove",item))
-      }
-    } else {
-        print(paste("Did not remove",item))
-    }
-  } else {
-     print(paste("Failed to copy",item)) 
-  }
+      if(file.remove(item)){print(paste("Removed",item))
+      }else{print(paste("Failed to remove",item))}
+    }else{print(paste("Did not remove",item))}
+  }else{print(paste("Failed to copy",item))}
 }
-
-
