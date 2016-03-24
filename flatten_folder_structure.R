44d767dbdf7e4e59b38c649d58a20ffb6c027bb1
Lewis-Peaty <lewis.peaty@gmail.com>
2016-03-24 15:29:07 +0800

    Can flatten folder by renaming or copying.
    Renaming is much quicker, but copying allows option to leave files in sub directories.

--- flatten_folder_structure.R
+++ flatten_folder_structure.R
@@ -1,9 +1,20 @@
 
 #From a defined folder, recursively access subfolders and move all files to top level
 folder <- "C:\\Users\\n043341\\Documents\\Documents\\Software\\R"
-remove_after_copying <- TRUE
 setwd(folder)
 
+#Renaming Version (moves files by renaming them- true story!)
+for(item in list.files(folder, recursive = TRUE)){
+  if(length(grep("/",item))==0){next} #Skip files in top level folder
+  s <- unlist(strsplit(item,"/"))
+  to <- paste(sep="",folder,"/",s[length(s)])
+  if(file.rename(from = item, to = to)){
+    print(paste("Successfully moved",item,"to",folder)) 
+  }else{print(paste("Failed to move",item))}
+}
+
+#Copying Version
+remove_after_copying <- FALSE
 for(item in list.files(folder, recursive = TRUE)){
   if(length(grep("/",item))==0){next} #Skip files in top level folder
   if(file.copy(from = item,to = folder)){
@@ -14,3 +25,7 @@ for(item in list.files(folder, recursive = TRUE)){
     }else{print(paste("Did not remove",item))}
   }else{print(paste("Failed to copy",item))}
 }
+
+
+
+
