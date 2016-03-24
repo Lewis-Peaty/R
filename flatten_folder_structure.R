1d2596777d0fa0b0f27b4c8e4f4e5431ba979512
Lewis-Peaty <lewis.peaty@gmail.com>
2016-03-24 15:10:06 +0800

    Create flatten_folder_structure.R

---
+++ flatten_folder_structure.R
@@ -0,0 +1,25 @@
+
+#From a defined folder, recursively access subfolders and move all files to top level
+folder <- "C:\\Users\\n043341\\Documents\\Documents\\Software\\R"
+remove_after_copying <- TRUE
+setwd(folder)
+
+for(item in list.files(folder, recursive = TRUE)){
+  if(length(grep("/",item))==0){next} #Skip files in top level folder
+  if(file.copy(from = item,to = folder)){
+    print(paste("Successfully copied",item,"to",folder)) 
+    if(remove_after_copying){
+      if(file.remove(item)){
+        print(paste("Removed",item))
+      } else{
+         print(paste("Failed to remove",item))
+      }
+    } else {
+        print(paste("Did not remove",item))
+    }
+  } else {
+     print(paste("Failed to copy",item)) 
+  }
+}
+
+
